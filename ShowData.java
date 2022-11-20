ed0bd4fe4740f9524ff74ffc4a0f9bec4708d94a
sakatashi <shiori.1002sakata@gmail.com>
2022-11-20 05:56:25 +0000

    [Add]showdata

---
+++ ShowData.java
@@ -0,0 +1,16 @@
+class ShowData{
+  public static void main(String[] args){
+    int num = 10;
+    double pi = 3.14;
+    char blood = 'A';
+    boolean fun = true;
+    
+    String msg ="頑張る！";
+    
+    System.out.println(num);
+    System.out.println(pi);
+    System.out.println(blood);
+    System.out.println(fun);
+    System.out.println(msg);
+  }
+}
