d15869582fd5b900b2eb33d46b146aee3972550a
sakatashi <shiori.1002sakata@gmail.com>
2022-11-23 05:29:35 +0000

    [Add]loopmessage

---
+++ LoopMessage.java
@@ -0,0 +1,10 @@
+class LoopMessage{
+  public static void main(String[] args){
+    //メッセージを１０回表示
+    int i = 1;
+    while(i <= 10){
+      System.out.println( i+"回目：フリーラーニングを広げたい！");
+      i++;
+    }
+  }
+}
