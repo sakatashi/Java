2c8d28ff4062078c472f0cc0f36309579d264d18
sakatashi <shiori.1002sakata@gmail.com>
2022-11-23 05:15:33 +0000

    [Add]circle

--- Circle.java
+++ Circle.java
@@ -7,7 +7,8 @@ class Circle{
     area = radius * radius * pi;
     
     //小数点第３位以降を切り捨て
-    
+    area = (int)(area * 100);
+    area = area / 100;
     
     System.out.println("半径：" + radius);
     System.out.println("面積" + area);
