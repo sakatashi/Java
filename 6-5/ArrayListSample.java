3776c1a00e4d25e3b301b9bd137f7264f7d5e183
sakatashi <shiori.1002sakata@gmail.com>
2022-12-07 02:39:58 +0000

    [Add]6-5

---
+++ 6-5/ArrayListSample.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class ArrayListSample{
+  public static void main(String[] args){
+    ArrayList<String> list = new ArrayList<String>();
+    list.add("Java入門講座");
+    list.add("ここまで");
+    list.add("よく頑張りましたね！");
+
+    for(int i = 0; i < list.size(); i++){
+      System.out.println(list.get(i));
+    }
+  }
+}
