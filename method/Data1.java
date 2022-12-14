21a64406eed4b659ad6088b340a2f4893a51530c
sakatashi <shiori.1002sakata@gmail.com>
2022-12-14 07:12:37 +0000

    [Add]data1

---
+++ method/Data1.java
@@ -0,0 +1,17 @@
+class Data1 {
+  public static void main(String[] args) {
+    // 引数に「Kate Jones」を渡してください
+    printData("Kate Jones");
+
+    // 引数に「John Christopher Smith」を渡してください
+    printData("John Christopher Smith");
+
+  }
+
+  // 引数を受け取るようにしてください
+  public static void printData(String printData) {
+    // 「私の名前は◯◯です」と出力されるように書き換えてください
+    System.out.println("私の名前は"+ printData +"です");
+
+  }
+}
